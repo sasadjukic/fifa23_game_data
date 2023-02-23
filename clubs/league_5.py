8f4c824d8f3daa7ba4b36cd6637a8b8cb6dc28c5
Sasa Djukic <djukicmsasa@gmail.com>
2023-02-23 10:06:42 +0100

    add league 5 helper file

---
+++ clubs/league_5.py
@@ -0,0 +1,23 @@
+
+
+all_teams = [
+    'Arsenal', 'Manchester City', 'Manchester United', 'Tottenham Hotspur', 'Newcastle United', 
+    'Fulham', 'Brighton & Hove Albion', 'Liverpool', 'Brentford', 'Chelsea', 'Aston Villa', 
+    'Crystal Palace', 'Nottingham Forest', 'Leicester City', 'Wolverhampton Wanderers', 
+    'Everton', 'AFC Bournemouth', 'West Ham United', 'Leeds United', 'Southampton',
+    'FC Barcelona', 'Real Madrid CF', 'Real Sociedad', 'Atlético de Madrid', 'Real Betis Balompié', 
+    'Rayo Vallecano', 'Athletic Club de Bilbao', 'RCD Mallorca', 'Villarreal CF', 'CA Osasuna', 
+    'Girona FC', 'Sevilla FC', 'RCD Espanyol de Barcelona', 'RC Celta de Vigo, Real Valladolid CF', 
+    'UD Almeria', 'Cádiz CF', 'Valencia CF', 'Getafe CF', 'Elche CF',
+    'FC Bayern München', 'Borussia Dortmund', '1. FC Union Berlin', 'Sport-Club Freiburg', 'RB Leipzig', 
+    'Eintracht Frankfurt', 'VfL Wolfsburg', 'Borussia Mönchengladbach', '1. FSV Mainz 05', 
+    'Bayer 04 Leverkusen', 'SV Werder Bremen', '1. FC Köln', 'FC Augsburg', 'VfB Stuttgart', 
+    'TSG Hoffenheim', 'VfL Bochum 1848', 'Hertha BSC', 'FC Schalke 04',
+    'Napoli', 'Inter', 'Roma', 'AC Milan', 'Lazio', 'Atalanta', 'Juventus', 'Bologna', 
+    'Torino F.C.', 'Udinese Calcio', 'AC Monza', 'Empoli', 'Lecce', 'Fiorentina', 'U.S. Sassuolo Calcio', 
+    'US Salernitana 1919', 'Spezia', 'Hellas Verona', 'U.C. Sampdoria', 'US Cremonese',
+    'Paris Saint-Germain', 'Olympique de Marseille', 'AS Monaco', 'Racing Club de Lens', 'Stade Rennais FC', 
+    'LOSC Lille', 'FC Lorient', 'OGC Nice', 'Olympique Lyonnais', 'Stade de Reims', 'Toulouse Football Club', 
+    'Clermont Foot 63', 'FC Nantes', 'Montpellier Hérault SC', 'RC Strasbourg Alsace', 'Stade Brestois 29', 
+    'ESTAC Troyes', 'AJ Auxerre', 'AC Ajaccio', 'Angers SCO'
+]
